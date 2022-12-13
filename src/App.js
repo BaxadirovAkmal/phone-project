712ac9f3e8202e37bf3668005e6496e56b257055
a.baxadirov <a.baxadirov@lebazar.uz>
2022-12-13 17:15:08 +0500

    refactor: remove unused items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
-
-function App() {
-  return (
-      <Routes>
-          <Route path='*' element={<MainLayout/>}/>
-      </Routes>
-  );
-}
-
-export default App;
+const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route path='*' element={<MainLayout/>}/>
+            </Routes>
+        </>
+    );
+};
+export default App
